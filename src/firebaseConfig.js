8939c1528128dbb12b294b9f608473b4b8ea9caa
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2025-02-02 06:38:17 +0530

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvmR2oHx-ynxBunRKXbZXM_c0p7P9PJ1o",
+  authDomain: "blessings-197f1.firebaseapp.com",
+  projectId: "blessings-197f1",
+  storageBucket: "blessings-197f1.firebasestorage.app",
+  messagingSenderId: "184058828678",
+  appId: "1:184058828678:web:b553f96e35dde3c4707e07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
