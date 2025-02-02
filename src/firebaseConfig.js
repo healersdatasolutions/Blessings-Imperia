c4dc42bc54063faa5f0ce500e1b0323bbc46d3c1
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2025-02-02 06:53:59 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth"; // Import getAuth to manage authentication
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and export it
+const auth = getAuth(app);
+export { auth }; // Export the auth object for use in other parts of your app
